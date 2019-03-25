eb6b9e8ece2d95a58572824ca6a0aeaebac1ce07
Rick Herrick <jrherrick@wustl.edu>
2019-03-25 14:56:31 -0500

    Created sample plugin to test edit issue with custom data types

---
+++ README.md
@@ -0,0 +1,16 @@
+# XNAT-6000 Plugin #
+
+This is a sample XNAT plugin project for reviewing the bug report in [XNAT-6000](https://issues.xnat.org/browse/XNAT-6000).
+
+To build the plugin, run the following command from within the source repo:
+
+```
+$ ./gradlew clean jar
+```
+
+You can find the deployable plugin jar in:
+
+```
+build/libs/cs-data-plugin-1.0.0-SNAPSHOT.jar
+```
+
---
+++ src/main/java/org/nrg/xnatx/cs/CsPlugin.java
@@ -0,0 +1,15 @@
+package org.nrg.xnatx.cs;
+
+import lombok.extern.slf4j.Slf4j;
+import org.nrg.framework.annotations.XnatDataModel;
+import org.nrg.framework.annotations.XnatPlugin;
+import org.nrg.xdat.om.CsCogscrdata;
+
+@XnatPlugin(value = "xnatxCsDataPlugin", name = "XNATX CS Data Plugin",
+            dataModels = {@XnatDataModel(value = CsCogscrdata.SCHEMA_ELEMENT_NAME,
+                                         code = "CRF",
+                                         singular = "CRF",
+                                         plural = "CRFs")})
+@Slf4j
+public class CsPlugin {
+}
